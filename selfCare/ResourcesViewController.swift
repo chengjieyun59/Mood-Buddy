9f6505b2761f3fa9e58fdd6729e8f7d39d3b0ef0
chengjieyun59 <chengjieyun59@gmail.com>
2017-07-31 12:19:12 -0700

    Finished resources
    Put 8 resources on the resources page. Used switch cases to open the correct link when user taps a row

--- selfCare/ResourcesViewController.swift
+++ selfCare/ResourcesViewController.swift
@@ -11,9 +11,9 @@ import UIKit
 class ResourcesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    let resources = ["National Suicide Hotline"]
-    let url1 = URL(string: "https://suicidepreventionlifeline.org/")!
+    let resources = ["1. National Suicide Hotline", "2. Depression Hotline", "3. Support for Sexual Assault Survivors", "4. Support for LGBTQIA", "5. 30 Day Meditation Challenge", "6. 30 Day Life-Changing Challenges", "7. Improve Relationships with Others", "8. Improve Quality of Life"]
 
+    /*
     func openURL() {
         if #available(iOS 10.0, *){
             UIApplication.shared.open(url1, options: [:], completionHandler: nil)
@@ -21,6 +21,7 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
             UIApplication.shared.openURL(url1)
         }
     }
+    */
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,9 +48,34 @@ class ResourcesViewController: UIViewController, UITableViewDelegate, UITableVie
         // Dispose of any resources that can be recreated.
     }
     
-    // tap cell to open url in safari
+    // tap cell to open respective URLs in Safari
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        openURL()
+        // openURL()
+        let url : NSURL?
+        switch indexPath.row{
+        case 0:
+            url = NSURL(string: "https://suicidepreventionlifeline.org/") //National Suicide Hotline
+        case 1:
+            url = NSURL(string: "https://www.mentalhelp.net/articles/depression-hotline/") //Depression Hotline
+        case 2:
+            url = NSURL(string: "https://www.rainn.org/national-resources-sexual-assault-survivors-and-their-loved-ones") //Support for Sexual Assault Survivors
+        case 3:
+            url = NSURL(string: "http://www.glbtnationalhelpcenter.org/") //Support for LGBTQIA
+        case 4:
+            url = NSURL(string: "https://www.youtube.com/playlist?list=PLF78_4TwdUj-XGrceHHlv2Rhz_aKB6wYs") //30 Day Meditation Challenge
+        case 5:
+            url = NSURL(string: "http://embracingsimpleblog.com/fifteen-30-day-challenge-ideas-will-change-life/") //30 Day Life-Changing Challenges
+        case 6:
+            url = NSURL(string: "http://optimallivingdaily.com/category/optimal-living-daily-relationships/") //Improve Relationships with Others
+        case 7:
+            url = NSURL(string: "http://optimallivingdaily.com/category/optimallivingdaily/") //Improve Quality of Life
+        default:
+            return;
+        }
+        if url != nil{
+            // UIApplication.shared.openURL(url! as URL)
+            UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
+        }
     }
 
     /*
